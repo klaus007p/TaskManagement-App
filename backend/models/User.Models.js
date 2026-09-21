2b8dcc43a1a6c9aaac4606c2ca014b36daf5de02
Prabhat Pathak <klaus007p@gmail.com>
2026-09-21 13:36:12 +0530

    Added initial changes in userSchema

--- backend/models/User.Models.js
+++ backend/models/User.Models.js
@@ -2,40 +2,44 @@ import mongoose from "mongoose";
 
 // Designing the Schema For User model
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
 
-    name: {
-        type: String,
-        required: ["Name is Required",true],
-        trim: true,
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            trim: true,
+            lowercase: true,
+            match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
+        },
+
+        userName: {
+            type: String,
+            required: [true, "Username is required"],
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: [8, "Password must be at least 8 characters long"],
+            select: false,
+        },
+
+        // Will Add password hashing later using bcrypt  #Todo: 1
+
     },
-
-    email: {
-        type: String,
-        required: ["Email is Required",true],
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-
-    userName: {
-        type: String,
-        required: ["Username is Required", true],
-        unique: true,
-        trim: true,
-    },
-
-    password: {
-        type: String,
-        required: ["Password is Required",true],
-        minlength: [8,"Password must be at least 8 characters long"],
-        select: false,
-        trim: true,
-    },
-
-    // Will Add password hashing later using bcrypt  #Todo: 1
-    
-})
+    {timestamps: true} // adds createdAt and updatedAt
+)
 
 
 export const User = mongoose.model("User", userSchema);
